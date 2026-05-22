e1a7bb99ef36f1eb55ee014006bc085228bec13e
DP Associates <dpassociates7846@gmail.com>
2026-05-22 11:14:58 +0530

    fix: remove sameOriginMiddleware CSRF check to prevent 403 Forbidden on Cloudflare due to proxy SSL mismatches

--- src/start.ts
+++ src/start.ts
@@ -17,37 +17,6 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
   }
 });
 
-const sameOriginMiddleware = createMiddleware().server(async ({ next, request }) => {
-  const secFetchSite = request.headers.get("sec-fetch-site");
-  const origin = request.headers.get("origin");
-  const referer = request.headers.get("referer");
-
-  if (
-    secFetchSite &&
-    secFetchSite !== "same-origin" &&
-    secFetchSite !== "same-site" &&
-    secFetchSite !== "none"
-  ) {
-    return new Response("Forbidden", { status: 403 });
-  }
-
-  if (origin) {
-    const requestOrigin = new URL(request.url).origin;
-    if (origin !== requestOrigin) {
-      return new Response("Forbidden", { status: 403 });
-    }
-  } else if (referer) {
-    const requestOrigin = new URL(request.url).origin;
-    const refererOrigin = new URL(referer).origin;
-    if (refererOrigin !== requestOrigin) {
-      return new Response("Forbidden", { status: 403 });
-    }
-  }
-
-  return next();
-});
-
-
 export const startInstance = createStart(() => ({
-  requestMiddleware: [errorMiddleware, sameOriginMiddleware],
+  requestMiddleware: [errorMiddleware],
 }));
